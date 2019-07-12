37e3242fa37ff44555fa4d162b0f2424088c0af8
Kyle <kylepw@gmail.com>
2019-07-12 23:38:30 +0900

    Solved LC3

--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,22 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
+function lengthOfLongestSubstring(s) {
+  var longest = '';
+  var current = '';
+  for (let i = 0; i < s.length; i++) {
+      if (current.includes(s[i])) {
+        if (current.length > longest.length) {
+          longest = current;
+        }
+        current = current.split(s[i])[1] += s[i];
+        console.log(current)
+      } else { current += s[i]; }
+  }
+  if (current.length > longest.length) {
+    longest = current;
+  }
+  return longest.length;
+}
 
 module.exports = lengthOfLongestSubstring;
