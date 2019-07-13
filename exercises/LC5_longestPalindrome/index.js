03e632fa14d0135e3928315295b86aff65f1dabd
Kyle <kylepw@gmail.com>
2019-07-13 20:13:15 +0900

    Solve LC5

--- exercises/LC5_longestPalindrome/index.js
+++ exercises/LC5_longestPalindrome/index.js
@@ -4,6 +4,26 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+function longestPalindrome(s) {
+  var longest = '';
+  var current = '';
+
+    function reverseString(s) {
+      let tempArray = s.split("");
+      tempArray = tempArray.reverse();
+      return tempArray.join("");
+    }
+
+  for (let start = 0; start < s.length; start++) {
+    current = '';
+    for (let end = start; end < s.length; end++) {
+      current += s[end];
+      if (current == reverseString(current) && current.length > longest.length) {
+        longest = current;
+      }
+    }
+  }
+  return longest;
+}
 
 module.exports = longestPalindrome;
